aac6f1418bfb87a24a43abbdbc35791d3deee34f
strazco <strazco@users.noreply.github.com>
2014-08-19 16:38:56 -0500

    Update example3.py

--- example3.py
+++ example3.py
@@ -1,18 +1,17 @@
 from xml.dom import minidom
 import urllib
 
-#url_str = 'http://www.newyorkfed.org/markets/omo/dmm/fftoXML.cfm?type=daily'
-url_str = 'http://40in20out.com/subscribers/messages2.xml'
-xml_str = urllib.urlopen(url_str).read()
+url_str = 'http://www.newyorkfed.org/markets/omo/dmm/fftoXML.cfm?type=daily'
+xml_str = urllib.urlopen(xml_str).read()
 xmldoc = minidom.parseString(xml_str)
 
-action = xmldoc.getElementsByTagName('VFPData')
+obs_values = xmldoc.getElementsByTagName('base:OBS_VALUE')
 # prints the first base:OBS_VALUE it finds
-print action[0].firstChild.nodeValue
+print obs_values[0].firstChild.nodeValue
 
 # prints the second base:OBS_VALUE it finds
-print action[1].firstChild.nodeValue
+print obs_values[1].firstChild.nodeValue
 
 # prints all base:OBS_VALUE in the XML doc
-for obs_val in action:
+for obs_val in obs_values:
     print obs_val.firstChild.nodeValue
